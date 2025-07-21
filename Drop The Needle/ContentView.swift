314d02c3708f636fa0e3f8e52f546a48d3caef57
kevknowscode <KM.Kairouz@gmail.com>
2025-07-20 23:06:48 -0400

    UI Background on simulator

--- Drop The Needle/ContentView.swift
+++ Drop The Needle/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            Image("dropNeedleBackground").resizable().scaledToFill().ignoresSafeArea()
+            
+            VStack{
+                
+                Text("Drop The Needle").padding(50)
+                Spacer()
+                
+            }
+            
+           // use the imported image as background image import to assetts
+            
+            // import the record player to assets
+            
+            //have a slider that indicates the music player
+            
+            //when the music is playing has the record spin and when you press pause the the record does not spin
+            
+            //have a text field that checks the song name
+            
+            
         }
-        .padding()
+        
+        
+        
+        
+        
     }
 }
 
